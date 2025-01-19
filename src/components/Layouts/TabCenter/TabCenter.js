8e04c1f3e962cab91cddf8793ac51c204ed54cfb
BBageon <rjsdn4403@gmail.com>
2025-01-19 22:19:30 +0900

    asd

--- src/components/Layouts/TabCenter/TabCenter.js
+++ src/components/Layouts/TabCenter/TabCenter.js
@@ -16,47 +16,14 @@ const TabCenter = ({
         console.log("바뀜", activeKey)
     };
 
-    const renderContent = () => {
-        if (activeKey === '0') {
-            return (
-                <div>
-                    {inProgress?.length > 0 ? (
-                        inProgress.map((item, idx) => (
-                            <div key={idx}>
-                                asd
-                            </div>
-                        ))
-                    ) : (
-                        <div>진행 중인 데이터가 없습니다</div>
-                    )}
-                </div>
-            );
-        } else if (activeKey === '1') {
-            return (
-                <div>
-                    {finish?.length > 0 ? (
-                        finish.map((item, idx) => (
-                            <div key={idx}>
-                                asd
-                            </div>
-                        ))
-                    ) : (
-                        <div>완료된 데이터가 없습니다</div>
-                    )}
-                </div>
-            );
-        }
-    };
     
     return (
         <div className='tab-center-container'>
             <Tabs
-                defaultActiveKey='1'
+                defaultActiveKey='0'
                 tabPosition='top'
-                onChange={handleTabChange}
                 items={items}
             />
-            {renderContent()}
         </div>
 
     );
--- src/routes/pages/Employee/Profile/ProfilePresenter.jsx
+++ src/routes/pages/Employee/Profile/ProfilePresenter.jsx
@@ -1,95 +1,133 @@
-import './Profile.css';
-import { EmployeeMainLayout, Tab, TabCenter } from 'components/Layouts';
-import test from './test.svg';
+    import './Profile.css';
+    import { EmployeeMainLayout, Tab, TabCenter } from 'components/Layouts';
+    import test from './test.svg';
+    import { Children } from 'react';
+import { ColumnHeightOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom';
 
-/**
-* 직원 업무 요약
-*/
-const TaskSummary = ({
-    total,
-    finish,
-    inProgress,
-}) => {
-    return (
-        <div className='task-container summary'>
-            <div className='task-summary bluebar'>
-                <span>총 수행 업무</span>
-                <span>{total?.length}</span>
+    /**
+    * 직원 업무 요약
+    */
+    const TaskSummary = ({
+        total,
+        finish,
+        inProgress,
+    }) => {
+        return (
+            <div className='task-container summary'>
+                <div className='task-summary bluebar'>
+                    <span>총 수행 업무</span>
+                    <span>{total?.length}</span>
+                </div>
+                <div className='task-summary yellowbar'>
+                    <span>진행 업무</span>
+                    <span>{inProgress?.length}</span>
+                </div>
+                <div className='task-summary greenbar'>
+                    <span>완료 업무</span>
+                    <span>{finish?.length}</span>
+                </div>
             </div>
-            <div className='task-summary yellowbar'>
-                <span>진행 업무</span>
-                <span>{inProgress?.length}</span>
+        )
+    };
+
+    /**
+     * 직원 정보
+     */
+    const EmployeeInfo = () => {
+        return (
+            <div className='employee-profile-header'>
+                <img src={test} />
+                <span className='employee-name'>이순신</span>
+                <span className='employee-email'>cleankong@gmail.com</span>
             </div>
-            <div className='task-summary greenbar'>
-                <span>완료 업무</span>
-                <span>{finish?.length}</span>
-            </div>
-        </div>
-    )
-};
+        )
+    };
 
-/**
- * 직원 정보
- */
-const EmployeeInfo = () => {
-    return (
-        <div className='employee-profile-header'>
-            <img src={test} />
-            <span className='employee-name'>이순신</span>
-            <span className='employee-email'>cleankong@gmail.com</span>
-        </div>
-    )
-};
+    /**
+     * 청소 내역 필터링
+     */
 
-/**
- * 청소 내역 필터링
- */
+    const ProfilePresenter = ({
+        total,
+        finish,
+        inProgress,
+        isLoading,
+    }) => {
+        const navigate = useNavigate();
+        
+        const ProfileRequestForm = ({data}) => {
+            return (
+                <div className='profile-request-form-wrap'>
+                    {data?.length > 0 ? (
+                        <div className='profile-request-form-list'>
+                            {data.map((item, idx) => (
+                                
+                                <div className='profile-request-form'
+                                    onClick={()=>{navigate(`/employee/requestinfo/${item?.request_clean.request_clean_id}`)}}
+                                >
+                                    <p className='profile-request-name'>{item?.request_clean.user_name ? item?.request_clean.user_name : "이름 없음"} 님</p>
+                                    <p>
+                                        {item?.request_clean.clean_address ? item?.request_clean.clean_address : "주소없음"}
+                                        {" "} 
+                                        {item?.request_clean.clean_address_detail ? item?.request_clean.clean_address_detail : "상세 주소없음"} 
+                                    </p>
 
-const ProfilePresenter = ({
-    total,
-    finish,
-    inProgress,
-    isLoading,
-}) => {
-    const Tabitems = [
-        {
-            label : "진행중",
-            key : 0,
-        },
-        {
-            label : "청소 완료",
-            key : 1,
-        },
-    ]
-    
-    if (isLoading) {
-        return null;
-    }
+                                </div>
+                            ))}
+                        </div>
+                    ) : (
+                        <div>요청 기록이 없습니다.</div>
+                    )}
+                    
+                </div>
+            )
+        }
+        const Tabitems = [
+            {
+                label: "진행중",
+                key: 0,
+                children: <ProfileRequestForm
+                    data={inProgress}
+                />,
+            },
+            {
+                label: "청소 완료",
+                key: 1,
+                children: <ProfileRequestForm
+                    data={finish}
+                />,
+            },
+        ]
 
-    return (
-        <EmployeeMainLayout
-            footer={true}
+        if (isLoading) {
+            return null;
+        }
 
-            isShowTop={true}
-            title={'프로필'}
-            notShowIcon={true}
-        >
-            <div className='employee-profile-container'>
-                <EmployeeInfo/>
-                <TaskSummary
-                    total={total}
-                    finish={finish}
-                    inProgress={inProgress}
-                />
-                <TabCenter
-                    items={Tabitems}
-                    finish={finish}
-                    inProgress={inProgress}
-                />
-            </div>
+        return (
+            <EmployeeMainLayout
+                footer={true}
 
-        </EmployeeMainLayout>
-    );
-};
+                isShowTop={true}
+                title={'프로필'}
+                notShowIcon={true}
+            >
+                <div className='employee-profile-container'>
+                    <EmployeeInfo />
+                    <TaskSummary
+                        total={total}
+                        finish={finish}
+                        inProgress={inProgress}
+                    />
+                    <TabCenter
+                        items={Tabitems}
+                        finish={finish}
+                        inProgress={inProgress}
+                    />
+                </div>
 
-export default ProfilePresenter;
+            </EmployeeMainLayout>
+        );
+    };
+
+    export default ProfilePresenter;
